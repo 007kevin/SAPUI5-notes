afade9c74e1f3d07145c1c4f1eda1ec82d60c298
Kevin Kim <kevin.kim03@sap.com>
2017-06-14 15:58:09 -0400

    step 20

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,30 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
-    "use strict";
-    return UIComponent.extend("sap.ui.demo.wt.Component", {
-        metadata : {	
-		    rootView: "sap.ui.demo.wt.view.App"
-	    },
-        init : function () {
-            // call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-            // set data model
-            var oData = {
-                recipient : {
-                    name : "World!!"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel);
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/demo/wt/controller/HelloDialog"
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName : "sap.ui.demo.wt.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
-        }
-    });
+], function (UIComponent, JSONModel, HelloDialog) {
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.wt.Component", {
+		metadata : {
+			manifest : "json"
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+			// set data model
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+		},
+
+		openHelloDialog : function () {
+			this._helloDialog.open();
+		}
+	});
 });
